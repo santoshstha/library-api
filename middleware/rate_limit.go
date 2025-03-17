b4e843c885b10af2e711108853f184d83b127c04
Santosh <santoshstha2010@gmail.com>
2025-03-17 16:06:04 +0545

    doc rate limiter added

--- middleware/rate_limit.go
+++ middleware/rate_limit.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"golang.org/x/time/rate"
 )
+//limit (the maximum number of requests allowed per second)
+//burst (the maximum number of requests that can be made at once, for example, 20 requests in a burst).
 
 func RateLimit(limit float64, burst int) func(http.Handler) http.Handler {
 	limiter := rate.NewLimiter(rate.Limit(limit), burst) // e.g., 10 req/s, burst of 20
